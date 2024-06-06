1e8fa9f34e4a1d6557ade2226e3487a90bf4dcf8
quangtt-rks <quangtt@rikkeisoft.com>
2024-06-06 08:53:10 +0700

    Re-sync with Thinh

--- app/user-login/user-login.component.js
+++ app/user-login/user-login.component.js
@@ -10,8 +10,8 @@ angular.
         $scope.phoneNumer = ''
         // save data on localsotrage
         $scope.myFunc = function () {
-          localStorage.setItem('passengerName', $scope.userName);
-          localStorage.setItem('passengerPhone', $scope.phoneNumber);
+          localStorage.setItem('phonecatApp.passengerName', $scope.userName);
+          localStorage.setItem('phonecatApp.passengerPhone', $scope.phoneNumber);
           $location.path('/passenger');
         }
       }

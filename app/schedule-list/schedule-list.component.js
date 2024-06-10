dd29fe058ad33d9557985b1a37aa174cc367f4a1
duongtv2-rks <duongtv2@rikkeisoft.com>
2024-06-10 09:10:46 +0700

    observer fetching data and search results on all pages

--- app/schedule-list/schedule-list.component.js
+++ app/schedule-list/schedule-list.component.js
@@ -8,34 +8,51 @@ angular.
       function ScheduleListController(Schedule) {
         var vm = this;
 
+        console.log(vm)
+
         var pageSize = 10;
         vm.currentPage = 1;
         vm.orderProp = 'line';
+        vm.totalPages = 0;
+        vm.searchKeyword = '';
 
-        Schedule.query(function(data) {
-          vm.schedules = data;
-        });
-
-        vm.numberOfPages = function() {
-          return Math.ceil(vm.schedules.length / pageSize);
-        };
-
-        vm.getCurrentPageSchedules = function() {
+        function fetchPageData() {
+          var filteredSchedules = vm.schedules.filter(function(schedule) {
+              return schedule.line.toLowerCase().includes(vm.searchKeyword.toLowerCase());
+          });
+      
           var startIndex = (vm.currentPage - 1) * pageSize;
           var endIndex = startIndex + pageSize;
-          return vm.schedules.slice(startIndex, endIndex);
+          vm.currentPageSchedules = filteredSchedules.slice(startIndex, endIndex);
+        }
+
+        vm.search = function() {
+          fetchPageData();
         };
 
+        // Subscribe to the schedule data stream
+        var scheduleSubscription = Schedule.schedules$.subscribe(function(schedules) {
+          vm.schedules = schedules;
+          vm.totalPages = Math.ceil(vm.schedules.length / pageSize);
+          fetchPageData();
+        });
+
         vm.nextPage = function() {
-          if (vm.currentPage < vm.numberOfPages()) {
+          if (vm.currentPage < vm.totalPages) {
             vm.currentPage++;
+            fetchPageData();
           }
         };
 
         vm.previousPage = function() {
           if (vm.currentPage > 1) {
             vm.currentPage--;
+            fetchPageData();
           }
+        };
+
+        vm.$onDestroy = function() {
+          scheduleSubscription.unsubscribe();
         };
       }
     ]

363143db41653fa650113a685ee9b0d3e8d31999
cuongbm3rikkeisoft <cuongbm3@rikkeisoft.com>
2024-06-17 17:38:59 +0700

    Update user sign in logic

--- app/user-sign-in/user-sign-in.component.js
+++ app/user-sign-in/user-sign-in.component.js
@@ -1,19 +1,44 @@
 'use strict';
 
-angular.
-  module('userSignIn').
-  component('userSignIn', {
-    templateUrl: 'user-sign-in/user-sign-in.template.html',
-    controller: ['$scope', '$location',
-      function UserSignInController($scope, $location) {
-        $scope.userName = '';
-        $scope.phoneNumer = '';
-        // Save data on Local Storage
-        $scope.myFunc = function () {
-          localStorage.setItem('phonecatApp.passengerName', $scope.userName);
-          localStorage.setItem('phonecatApp.passengerPhone', $scope.phoneNumber);
-          $location.path('/passenger');
-        };
+angular.module('userSignIn').component('userSignIn', {
+  templateUrl: 'user-sign-in/user-sign-in.template.html',
+  controller: ['$location', class UserSignInController {
+    constructor($location) {
+      this.$location = $location;
+
+      this.username = '';
+      this.password = '';
+    }
+
+    submit() {
+      // Hard code credentials for testing purpose
+      const driverCredentials = {
+        username: 'driver',
+        password: 'driver',
+      };
+      const passengerCredentials = {
+        username: 'passenger',
+        password: 'passenger'
+      };
+      const userSignInCredentials = {
+        username: this.username,
+        password: this.password
+      };
+
+      if (_.isEqual(driverCredentials, userSignInCredentials)) {
+        userSignInCredentials.userType = 'driver';
+      } else if (_.isEqual(passengerCredentials, userSignInCredentials)) {
+        userSignInCredentials.userType = 'passenger';
+      } else {
+        alert('Invalid credentials');
+        return;
       }
-    ]
+
+      for (const [key, value] of Object.entries(userSignInCredentials)) {
+        localStorage.setItem(key, value);
+      }
+
+      this.$location.path('/passenger');
+    }
+  }]
 });
